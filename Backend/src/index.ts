b837ca8b8dad6f1822c4d6d0b037b172593f9eef
ahmedrazasalaamtakaful <ahmed.raza@salaamtakaful.com>
2026-08-19 23:02:32 -0700

    Cor update add domain

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -60,6 +60,7 @@ app.use(cors({
       'https://manpasand-pos-t623.vercel.app',
       'https://manpasand-pos-beta.vercel.app',
       'https://sarwat-traders-pos-bo8m.vercel.app',
+      'https://aceorbit-pos-demo.vercel.app',
       'http://localhost:3000',
       'http://localhost:3001',
       'http://localhost:5173',
